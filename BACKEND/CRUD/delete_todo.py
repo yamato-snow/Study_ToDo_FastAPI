e4a813374f817c5a6d7d16b87350e37af343945f
yamato-snow <yamato2016baseball@icloud.com>
2023-12-20 05:59:28 +0900

    メイン関数と削除関数のコメントを追加

--- BACKEND/CRUD/delete_todo.py
+++ BACKEND/CRUD/delete_todo.py
@@ -1,15 +1,23 @@
+# このファイルにdelete_todolist関数を記述しています。
+# この関数は、todolistテーブルからデータを削除する関数です。
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import text
 import datetime
 
+# todolistテーブルからデータを削除する関数です。
 def delete_todolist(deadline:datetime.date,todo:str,priority:int,genre:str,completed_flag:bool,nowdate:datetime.date,db:Session):
+    # todolistテーブルからデータを削除します。
     sql=text('delete from todolist where "deadline"=%s and "todo"=%s and \n\
              "priority"=%s and "genre"=%s and "completed_flag"=%s and "nowdate"=%s;'\
              %(repr(deadline),repr(todo),repr(priority),repr(genre),repr(completed_flag),repr(nowdate)))
+    # SQLを実行します。
     db.execute(sql)
+    # 保存します。
     db.commit()
 
+    # todolistテーブルのデータを全て取得します。
     sql2=text('select * from todolist order by "deadline" asc')
+    # SQLを実行します。
     result=db.execute(sql2).all()
-
+    # 取得したデータを返します。
     return result
--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,18 +1,32 @@
+# このソースコードを実行すると、サーバーが起動します。
+# サーバーが起動したら、ブラウザで http://localhost:8000/docs にアクセスしてください。
+# そうすると、Swagger UIが表示されます。
+# Swagger UIを使うと、APIの仕様を確認したり、APIを実行したりすることができます。
+# また、 http://localhost:8000/redoc にアクセスすると、ReDocが表示されます。
+# ReDocもSwagger UIと同様に、APIの仕様を確認したり、APIを実行したりすることができます。
+# どちらも同じことができるので、好きな方を使ってください。
+
 from fastapi import FastAPI
 from ROUTER import get_todo,insert_todo,delete_todo,update_todo
 
 app=FastAPI()
 
+# このAPIは、{"Hello": "World"}というJSONを返します。
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
+# このAPIは、{"Hello": 20}というJSONを返します。
 @app.get("/helloworld")
 def read_root():
     hello = 10 + 10
     return {"Hello": hello}
 
+# get_todo.pyのrouterを読み込みます。
 app.include_router(router=get_todo.router)
+# insert_todo.pyのrouterを読み込みます。
 app.include_router(router=insert_todo.router)
+# delete_todo.pyのrouterを読み込みます。
 app.include_router(router=delete_todo.router)
+# update_todo.pyのrouterを読み込みます。
 app.include_router(router=update_todo.router)
